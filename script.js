fc8d737b013617628a141905b9561a1fac68d76f
Lucas Dias <96730880+Lucas-4@users.noreply.github.com>
2022-09-22 14:41:30 -0300

    code refactoring

--- script.js
+++ script.js
@@ -1,58 +1,55 @@
-var lat;
-var lon;
-document.querySelector("#submit").addEventListener("click", getlocation);
-
-function getlocation(){
-    if(document.querySelector('#city').value=="" || document.querySelector('#state').value==""){
+function validateInput(){
+    if(document.querySelector('#city').value.trim().length == 0 || document.querySelector('#state').value.trim().length == 0){
         document.querySelector(".result").innerHTML = "";
-
         document.querySelector("#info").innerHTML = "XºC";
         document.querySelector('.alert').innerText = "Por favor, preencha todos os campos!"
-    } else{
-        document.querySelector('.alert').innerText = ""
-        var city = document.querySelector("#city").value;
-        var state = document.querySelector("#state").value;
-        var xhr = new XMLHttpRequest();
-        var url = "https://api.openweathermap.org/geo/1.0/direct?q=" + city + "," + state + ",BR&limit=1&appid=e90cbf3e3c1c947ae8bf290da980f6db";
-        xhr.onreadystatechange = function (){
-            if(xhr.readyState==4 && xhr.status==200){
-                var str = xhr.responseText;
-                var obj = JSON.parse(str);
-                console.log(obj);
-                try{
-                    lat = obj[0].lat;
-                    lon = obj[0].lon;
-                } catch{
-                    lat = "x";
-                    lon = "x";
-                }
-                if(lat!="x"){
-                    getTemp();
-                } else{
-                    document.querySelector(".result").innerHTML = "Local não encontrado.";
-                    document.querySelector("#info").innerHTML = "XºC";
-                }
-                
-            }
-        }
-        xhr.open("GET", url);
-        xhr.send();
+        return false;
+    }else {
+        document.querySelector('.alert').innerText = "";
+        return true
     }
 }
 
-function getTemp(){
-    var xhr = new XMLHttpRequest();
-    var url = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&units=metric&appid=e90cbf3e3c1c947ae8bf290da980f6db";
-    xhr.onreadystatechange = function (){
-        if(xhr.readyState==4 && xhr.status==200){
-            var str = xhr.responseText;
-            var obj = JSON.parse(str);
-            console.log(obj);
-            console.log(lat);
-            document.querySelector(".result").innerHTML = "Mostrando resultado para: " + obj.name + ".";
-            document.querySelector("#info").innerHTML = obj.main.temp +"ºC";
+
+async function getLocation(){
+    if(validateInput()){
+        let city = document.querySelector("#city").value;
+        let state = document.querySelector("#state").value;
+        let url = "https://api.openweathermap.org/geo/1.0/direct?q=" + city + "," + state + 
+        ",BR&limit=1&appid=e90cbf3e3c1c947ae8bf290da980f6db";
+        
+        const response = await fetch(url)
+
+        const data = await response.json();
+        console.log(data);
+
+        if(data.length == 0){
+            throw new Error('Location not found');
+        }
+
+        return {
+            lat: data[0].lat,
+            lon: data[0].lon,
+            name: data[0].name
         }
     }
-    xhr.open("GET", url);
-    xhr.send();
 }
+
+async function getWeather(){
+    try{
+        const coords = await getLocation();
+        console.log(coords);
+        const url = "https://api.openweathermap.org/data/2.5/weather?lat=" + coords.lat + "&lon=" + coords.lon + "&units=metric&appid=e90cbf3e3c1c947ae8bf290da980f6db";
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data);
+        document.querySelector("#info").innerHTML =  data.main.temp +" ºC";
+        document.querySelector(".result").innerHTML = "Mostrando resultado para: " + coords.name + ".";
+        
+    }catch(e){
+        document.querySelector("#info").innerHTML =  "";
+        document.querySelector(".result").innerHTML = e;
+    }
+}
+
+document.querySelector("#submit").addEventListener("click", getWeather);
